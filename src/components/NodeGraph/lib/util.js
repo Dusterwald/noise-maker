5bb93cec0aa3e8e1baa8cfb6d0a794db5e898db1
Sebastian Dusterwald <sebastian@dusterwald.com>
2020-06-01 00:53:08 +1200

    Styling improvements

--- src/components/NodeGraph/lib/util.js
+++ src/components/NodeGraph/lib/util.js
@@ -1,15 +1,17 @@
 /* not bound to style, should be computed */
 
 export function computeInOffsetByIndex(x, y, index) {
-  const outx = x + 15;
-  const outy = y + 47 + (index * 20);
+  const outx = x + 17;
+  //const outy = y + 47 + (index * 20);
+  const outy = y + 5 + (index * 20);
 
   return { x: outx, y: outy };
 }
 
 export function computeOutOffsetByIndex(x, y, index) {
   const outx = x + 166;
-  const outy = y + 49 + (index * 22);
+  //const outy = y + 49 + (index * 22);
+  const outy = y + 28 + (index * 22);
 
   return { x: outx, y: outy };
 }
